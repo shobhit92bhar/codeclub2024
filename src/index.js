e90c40da7d276e0616d4410aee5724a2d4a9afdc
Shobhit Bhardwaj <shobhit@cloudflare.com>
2024-04-24 17:48:53 -0400

    POST update

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-export default {
-	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
+// src/index.js
+var src_default = {
+  async fetch(request, env, ctx) {
+    if(request.url == "https://www.cloudflareworkers.com/test") {
+           return new Response('Hello worker!', {
+               headers: {
+                   'content-type': 'text/plain',
+               },
+           });
+       }
+       else if(request.method == 'POST') {
+       		// Sample JSON data to send as response
+      		const jsonData = {
+        		message: 'POST',
+        		data: 'request'
+      		};
+      		return new Response(JSON.stringify(jsonData), {
+       			headers: {
+       				'content-type': 'application/json',
+       			},
+       		});
+       }
+       else{
+           return new Response('Error Worker! Wrong URL', {
+               headers: {
+                   'content-type': 'text/plain',
+               },
+           });
+       }
+  }
 };
+export {
+  src_default as default
+};
+//# sourceMappingURL=index.js.map
